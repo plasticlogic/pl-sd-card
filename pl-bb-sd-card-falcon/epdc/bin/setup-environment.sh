d3a96bdda3ceda68c99bf6cd56686884eba6f0e0
Lars Görner <lars.goerner@plasticlogic.com>
2022-04-06 08:50:16 +0200

    replaced outdated scripts

--- pl-bb-sd-card-falcon/epdc/bin/setup-environment.sh
+++ pl-bb-sd-card-falcon/epdc/bin/setup-environment.sh
@@ -2,10 +2,14 @@
 set -e
 #comment out for use with Falcon
 #echo BB-PL-SPIDEV0 > /sys/devices/bone_capemgr.*/slots
-echo BB-PL-SPIDEV1 > /sys/devices/bone_capemgr.*/slots
-export PATH=/boot/uboot/epdc/bin:$PATH
+#echo BB-PL-SPIDEV1 > /sys/devices/bone_capemgr.*/slots
+#export PATH=/boot/uboot/epdc/bin:$PATH
+export PATH=$PATH:/boot/uboot/epdc/bin
 sleep 3
-epdc-app -start_epdc 0 1
-sleep 3
-slideshow-start.sh
+start-epdc.sh 1 1
+epdc-app -update_image /var/www/pl_dsp_ctl/images/qr_code.png
+#sleep 3
+#manual_slideshow.sh --first
+#slideshow-start.sh
 exit 0
+
