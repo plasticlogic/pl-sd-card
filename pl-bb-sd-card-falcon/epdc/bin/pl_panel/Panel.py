391510f4174f46f6218511156fe50f0cc7a78909
Lars Görner <lars.goerner@plasticlogic.com>
2022-06-08 11:36:15 +0200

    Fixed clear update + Fixed update

--- pl-bb-sd-card-falcon/epdc/bin/pl_panel/Panel.py
+++ pl-bb-sd-card-falcon/epdc/bin/pl_panel/Panel.py
@@ -3,6 +3,7 @@ Panel class module.
 """
 
 #from PIL import Image
+import subprocess
 import os
 import shutil
 
@@ -41,15 +42,12 @@ class Panel:
                 """
                 Clear the whole panel.
                 """
-                clear_buffer_path = os.path.join(POST_BUFFER_FOLDER, "clear")
-                if (not os.path.exists(clear_buffer_path)):
-                        os.mkdir(clear_buffer_path)
-                        raw_img_data = bytearray([0xFF] * 1280 * 960)
-                        for dsp_idx in range(len(self.__displays)):
-                                raw_img_path = os.path.join(clear_buffer_path, f'{dsp_idx:03d}.raw')
-                                raw_img_file = open(raw_img_path, "wb")
-                                raw_img_file.write(raw_img_data)
-                                raw_img_file.close()
+                raw_img_data = bytearray([0xFF] * 1280 * 960)
+                for dsp_idx in range(len(self.__displays)):
+                        raw_img_path = os.path.join(POST_BUFFER_FOLDER, f'{dsp_idx:03d}.raw')
+                        raw_img_file = open(raw_img_path, "wb")
+                        raw_img_file.write(raw_img_data)
+                        raw_img_file.close()
 
 
                 for display in self.__displays:
@@ -57,67 +55,56 @@ class Panel:
                         display.clear()
                         display.disable()
 
+                self.__copy_post_to_pre()
+
         def update(self, update_folder: str) -> None:
                 """
                 Update panel.
                 """
                 if (not os.path.isdir(update_folder)):
                         return
-
-                saved_image_names = os.listdir(POST_BUFFER_FOLDER)
-                new_image_name = os.path.basename(update_folder)
-                new_post_image_folder = os.path.join(POST_BUFFER_FOLDER, new_image_name)
-
-                if (not new_image_name in saved_image_names):
-                        os.mkdir(new_post_image_folder)
-                        self.__prepare_post_buffer(new_post_image_folder, update_folder)
                 
-                image_list = os.listdir(new_post_image_folder)
-                panel_elements = max(self.__displays.count(), image_list.count())
+                image_list = os.listdir(update_folder)
+                print(image_list)
+                panel_elements = max(len(self.__displays), len(image_list))
 
+                img_idx = 0
+                for image in image_list:
+                        image_path = os.path.join(update_folder, image)
+                        self.__copy_convert_image(image_path, img_idx)
+                        img_idx += 1
+
+                post_buffer_files = os.listdir(POST_BUFFER_FOLDER)
+                pre_buffer_files = os.listdir(PRE_BUFFER_FOLDER)
                 for display_idx in range(panel_elements):
+                        pre_file = os.path.join(PRE_BUFFER_FOLDER, pre_buffer_files[display_idx])
+                        post_file = os.path.join(POST_BUFFER_FOLDER, post_buffer_files[display_idx])
+                        print("PRE: ", pre_file, "POST: ", post_file)
                         current_display = self.__displays[display_idx]
                         current_display.enable()
-                        current_display.update(image_list[display_idx])
+                        current_display.write_pre_buffer(pre_file)
+                        current_display.update(post_file)
                         current_display.disable()
 
-                self.__copy_post_to_pre(new_post_image_folder)
-
-
-        def __prepare_post_buffer(self, new_post_folder: str, image_folder: str) -> None:
-                """
-                Create new post image folder and convert related images
-                """
-                folder_items = os.listdir(image_folder)
-                image_idx = 0
-                for item in folder_items:
-                        if (os.path.isfile(item)):
-                                img_path = os.path.join(image_folder, item)
-                                self.__copy_convert_image(img_path, new_post_folder, image_idx)
-                                image_idx += 1
+                self.__copy_post_to_pre()
         
-        def __copy_convert_image(self, img_path: str, target_folder_path: str, idx: int) -> None:
+        def __copy_convert_image(self, img_path: str, idx: int) -> None:
                 """
                 Convert image into raw format and copy it into post buffer
                 """
-                #img = Image.open(img_path, mode="r")
-                #dest_file_path = os.path.join(target_folder_path, str(idx) + ".raw")
-                #dest_file = open(dest_file_path, "wb")
+                target_path = os.path.join(POST_BUFFER_FOLDER, f'{idx:03d}.raw')
 
-                #img.convert("RGB")
-                #pixels = list(img.getdata())
-                #dest_file.write(pixels)
-                return
+                print("SRC: ", img_path, "DST: ", target_path)
+                subprocess.call(["png2cfa", "-i", img_path, "-o", target_path])
 
 
-        def __copy_post_to_pre(self, post_folder_path: str) -> None:
+        def __copy_post_to_pre(self) -> None:
                 """
                 Copy current post buffer content into pre buffer.
                 """
-                if (os.path.exists(post_folder_path) and os.path.isdir(post_folder_path)):
-                        for folder_item in os.listdir(post_folder_path):
-                                src_path = os.path.join(post_folder_path, folder_item)
-                                dest_path = os.path.join(PRE_BUFFER_FOLDER, folder_item)
+                for folder_item in os.listdir(POST_BUFFER_FOLDER):
+                        src_path = os.path.join(POST_BUFFER_FOLDER, folder_item)
+                        dest_path = os.path.join(PRE_BUFFER_FOLDER, folder_item)
 
-                                if (os.path.isfile(src_path)):
-                                        shutil.copyfile(src_path, dest_path)
+                        if (os.path.isfile(src_path)):
+                                shutil.copyfile(src_path, dest_path)
