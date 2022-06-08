6465e41985c8e99334f42aa8bba381ff659d8af5
Lars Görner <lars.goerner@plasticlogic.com>
2022-06-08 15:10:47 +0200

    removed epdc start

--- pl-bb-sd-card-falcon/epdc/bin/setup-environment.sh
+++ pl-bb-sd-card-falcon/epdc/bin/setup-environment.sh
@@ -5,9 +5,9 @@ set -e
 #echo BB-PL-SPIDEV1 > /sys/devices/bone_capemgr.*/slots
 #export PATH=/boot/uboot/epdc/bin:$PATH
 export PATH=$PATH:/boot/uboot/epdc/bin
-sleep 3
-start-epdc.sh 1 1
-epdc-app -update_image /var/www/pl_dsp_ctl/images/qr_code.png
+#sleep 3
+#start-epdc.sh 1 1
+#epdc-app -update_image /var/www/pl_dsp_ctl/images/qr_code.png
 #sleep 3
 #manual_slideshow.sh --first
 #slideshow-start.sh
