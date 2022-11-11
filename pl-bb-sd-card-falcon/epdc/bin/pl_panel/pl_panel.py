f2f8cd7300c58a75b8dabd20db6faf3a473cc9ff
Lars Görner <lars.goerner@plasticlogic.com>
2022-11-11 11:19:16 +0100

    Added file logging

--- pl-bb-sd-card-falcon/epdc/bin/pl_panel/pl_panel.py
+++ pl-bb-sd-card-falcon/epdc/bin/pl_panel/pl_panel.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+import logging
 
 import parse
 import Panel
@@ -30,14 +31,35 @@ def execute(args) -> None:
         if args.update_all:
                 pl_panel.update(args.update_all)
 
+def setup_logger() -> None:
+        rfh = logging.handlers.RotatingFileHandler(
+                filename="logging.out",
+                mode='a',
+                maxBytes=5*1024*1024,
+                backupCount=2
+                encoding="utf-8",
+                delay=0
+        )
+
+        logging.basicConfig(
+                level=logging.DEBUG,
+                format="%(asctime)s %(name)-25s %(levelname)-8s %(message)s",
+                datefmt="%y-%m-%d %H:%M:%S",
+                handlers=[ rfh ]
+        )
+
 def main() -> None:
+        setup_logger()
+        logger = logging.getLogger('main')
         try:
+                logger.debug("Start parsing")
                 parser = parse.init_argparse()
                 args = parser.parse_args()
                 sys.stdout.flush()
+                logger.debug("Start process")
                 execute(args)
         except:
-                print("ERROR: execution failed.")
+                logger.error("ERROR: execution failed.")
 
 if __name__ == "__main__":
         main()
