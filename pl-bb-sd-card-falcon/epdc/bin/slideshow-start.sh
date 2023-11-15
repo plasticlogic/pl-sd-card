3eb0c80e6c140736e8d14cdc87709d29a1e11fba
pljmh <46564572+pljmh@users.noreply.github.com>
2023-11-15 10:27:31 +0100

    Sync with pl-bb-sd-card and network drive

--- pl-bb-sd-card-falcon/epdc/bin/slideshow-start.sh
+++ pl-bb-sd-card-falcon/epdc/bin/slideshow-start.sh
@@ -1,7 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
-
 # do not run the slideshow if it is disabled
 if [ -e /var/tmp/slideshow-disabled ]; then
     echo "Slideshow disabled"
@@ -11,21 +10,34 @@ fi
 # remove the file which is used to stop the slideshow
 rm -f /var/tmp/slideshow-stop
 
+declare -a slideShowArray
+declare slideShowFolder
+
+slideShowFolder=$(cat /boot/uboot/config.txt)
+/boot/uboot/epdc/bin/slideshow-select.sh "${slideShowFolder#*=}"
+
+slideShowArray=($(ls /var/tmp/slideshow/*png))
+
 # persist cached changes of file system
 sync
 
 # run the slideshow
 while true; do
-for f in /var/tmp/slideshow/*.png; do
-if [ -e /var/tmp/slideshow-stop ]
-then
-echo "Stopping slideshow"
-exit 1
-fi
-epdc-app -update_image $f > /dev/null
-#epview -i 2 -r auto -b black "$f"
-#epview -i 2 "$f"        
-sleep 3
-done
+    for f in ${slideShowArray[@]}
+    do
+        if [ -e /var/tmp/slideshow-stop ]
+        then
+            echo "Stopping slideshow"
+            exit 1
+        fi
+	if [ "${slideShowFolder#*=}" = "D054_T2.1" ]
+	then
+            echo "ACEP Clear"
+            epdc-app -update_image $f 1 > /dev/null 
+	fi
+        epdc-app -update_image $f > /dev/null
+
+        sleep 5
+    done
 done
 exit 0
